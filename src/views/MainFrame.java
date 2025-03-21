68832dc677e10508a9de7621635cf73872482a6d
Melvyn B <melvyn.bauvent@etu.univ-lyon1.fr>
2025-03-21 15:11:21 +0100

    feat : ajout layout basique

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -12,20 +12,18 @@ public class MainFrame extends JFrame {
     private void build() {
         this.contentPane = new JPanel();
         this.contentPane.setPreferredSize(new Dimension(800, 600));
+        this.contentPane.setLayout(new BorderLayout());
 
-        this.board = new VChessBoard(600, 400);
-        this.contentPane.add(board);
+        this.board = new VChessBoard(600);
+        this.contentPane.add(board, BorderLayout.CENTER);
 
-        /*
         this.label = new JLabel("Super jeu d'échecs");
-        this.contentPane.add(label);
-
-         */
+        this.contentPane.add(label, BorderLayout.NORTH);
 
         this.setContentPane(this.contentPane);
 
         this.setTitle("Échecs");
-        this.setSize(800, 600);
+        this.setSize(800, 650);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
--- src/views/VChessBoard.java
+++ src/views/VChessBoard.java
@@ -6,14 +6,11 @@ import java.awt.*;
 public class VChessBoard extends JPanel {
     public static final int CHESS_BOARD_SIZE = 8;
 
-    private int height;
-    private int width;
+    private int size;
 
-    public VChessBoard(int height, int width) {
-        this.height = height;
-        this.width = width;
-        this.setPreferredSize(new Dimension(width, height));
-        this.repaint();
+    public VChessBoard(int size) {
+        this.size = size;
+        this.setPreferredSize(new Dimension(size, size));
     }
     @Override
     public void paintComponent(Graphics g) {
@@ -21,10 +18,8 @@ public class VChessBoard extends JPanel {
     }
 
     private void drawBaseBoard(Graphics g) {
-        g.setColor(Color.YELLOW);
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
 
-        int caseSize = this.getWidth() / CHESS_BOARD_SIZE;
+        int caseSize = this.size / CHESS_BOARD_SIZE;
         for (int y = 0; y < CHESS_BOARD_SIZE; y++) {
             for (int x = 0; x < CHESS_BOARD_SIZE; x++) {
                 if ((x+y) % 2 == 0) {
